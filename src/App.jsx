927c0911d84bb6eaa8298f245cc6e8b4319057cb
mir-abdullah-mahammad-lab <mirabdullahmahammad@gmail.com>
2026-04-18 20:51:44 +0600

    Assingment stated

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+// import { useState } from 'react'
+import './App.css'
+
+function App() {
+  // const [count, setCount] = useState(0)
+
+  return (
+    <> 
+    <h1 className='text-5xl text-center bg-amber-500'>Sa: </h1>
+    <button className='btn btn-success' >press here to knwo me</button>
+    
+    </>
+  )
+}
+
+export default App
